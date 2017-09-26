2c72abeed3dba97d71412440dc4ea6ed420a6368
Unknown <archambaultalexandre@outlook.com>
2017-09-26 11:16:09 -0400

    Creation Controller Profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,10 +45,11 @@
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
+
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->first_name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
@@ -63,10 +64,23 @@
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
+
+                                    <li>
+                                        <a href="{{ route('profile.edit',Auth::user()->id) }}">
+                                            Profile
+                                        </a>
+
+                                    </li>
                                 </ul>
+                            </li>
+                            <li>
+                                <a href="{{ route('home') }}" class="" >
+                                    Dashboard
+                                </a>
                             </li>
                         @endif
                     </ul>
+
                 </div>
             </div>
         </nav>
