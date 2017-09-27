6d2a5c5c16bd05a6a6aa23374b6ed5704397ae23
Unknown <snow_blooded08@hotmail.fr>
2017-09-27 14:46:04 -0400

    Merge branch 'Sam' into dev
    Creation des controllers commandes et ventes.
    Creation de leurs routes
    Creation de leur contenu hardcoder

---
+++ resources/views/commandes/commandes_live.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Numero Commande</th>
+                <th>Nombre d'articles commander</th>
+                <th>Montant total</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td>
+                <td>4</td>
+                <td>12.50$</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>1</td>
+                <td>4.50$</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>2</td>
+                <td>9.00$</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
