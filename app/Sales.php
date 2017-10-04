84530ab2ce476534ec356ad2274e7f953503245b
Unknown <snow_blooded08@hotmail.fr>
2017-10-04 13:02:36 -0400

    f

--- app/Sales.php
+++ app/Sales.php
@@ -9,11 +9,10 @@ class Sales extends Model
     protected $table = 'sales';
     protected $primaryKey = 'id';
 
-<<<<<<< Updated upstream
-=======
+
     public function saleitems()
     {
         return $this->hasMany('App\SaleItem');
     }
->>>>>>> Stashed changes
+
 }
