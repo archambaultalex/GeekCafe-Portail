762c467fd4d51043715125f6afb8d46c28f5b2ba
Unknown <archambaultalexandre@outlook.com>
2017-11-15 15:22:00 -0500

    Merge branch 'dev' into Alex
    Commandes page refresh

--- resources/views/Commandes/show_commandes.blade.php
+++ resources/views/Commandes/show_commandes.blade.php
@@ -10,7 +10,39 @@ use App\ItemSize;
     Commandes
     @endsection
 @section('content')
-   <iframe style="width:100%;height:500px;border:none;" src="http://geekcafe.dev/test2">
+    <iframe id="xIfrWidget0" scrolling="no" style="width:100%;min-height:4000px;border:none;display:none" src="http://geekcafe.dev/test2"></iframe>
+    <iframe id="xIfrWidget1" scrolling="no" style="width:100%;min-height:4000px;border:none;display:none" src="http://geekcafe.dev/test2"></iframe>
 
-   </iframe>
+    <script>
+        let ifrNo = 0;
+        let ifrHidden;
+        let ifr;
+
+        function swap() {
+
+            ifr = document.getElementById('xIfrWidget' + ifrNo);
+            ifrNo = 1 - ifrNo;
+            ifrHidden = document.getElementById('xIfrWidget' + ifrNo);
+
+            ifr.onload = null;
+            ifrHidden.onload = function() {
+
+                ifr.style.display = 'none';
+                ifrHidden.style.display = 'block';
+
+            }
+            ifrHidden.src = "http://geekcafe.dev/test2";
+
+        }
+
+        setInterval(function () {
+            swap();
+        }, 2000)
+    </script>
+
+    <style>
+        #test{
+            height:100% !important;
+        }
+    </style>
 @endsection
