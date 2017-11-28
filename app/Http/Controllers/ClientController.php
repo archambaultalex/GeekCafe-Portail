ebf41374688ae4981ad6e7952fab0af3db81eb10
Unknown <archambaultalexandre@outlook.com>
2017-11-27 21:58:41 -0500

    add where where

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -16,7 +16,7 @@ class ClientController extends Controller
 
     public function index()
     {
-        $client = Client::all()->where('is_employee',0);
+        $client = Client::all()->where('is_employee','==',0)->where('is_admin','==',0);
         return view('client.show_clients_ios',compact('client'));
     }
 }
