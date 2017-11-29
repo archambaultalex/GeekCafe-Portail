93f6c985d84761a8c899c5b8eb6130a7aeafb941
Unknown <archambaultalexandre@outlook.com>
2017-11-29 01:15:46 -0500

    branches

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -20,11 +20,44 @@ class BranchController extends Controller
 
     public function create()
     {
+
         return view('Branches.create_branch');
     }
 
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'name' => 'required',
+            'city' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'fax' => 'required',
+            'phone_no_fee' => 'required',
+        ]);
+
+        $client = new \GuzzleHttp\Client();
+
+        $res = $client->get('http://maps.google.com/maps/api/geocode/json?address='.$request->address.'&sensor=false');
+        $location = json_decode($res->getBody(), true)['results'][0]['geometry']['location'];
+
+        if(isset($request->image)) {
+            $encoded = base64_encode(file_get_contents($request->image->getrealpath()));
+            $uniquid = uniqid();
+            Image::create(['id'=>$uniquid,'image'=>$encoded]);
+        }
+
+        Branch::create([
+            'name' => $request->name,
+            'city' => $request->city,
+            'address' => $request->address,
+            'email' => $request->email,
+            'coordinates' => $location['lat'].','.$location['lng'],
+            'phone' => $request->phone,
+            'fax' => $request->fax,
+            'phone_no_fee' => $request->phone_no_fee,
+            'image_id' => $uniquid,
+        ]);
+
         return redirect ('branches');
     }
 
